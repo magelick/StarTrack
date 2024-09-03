5ef6b5bd9b450318199399b22785f720082cd722
magelick <fcdm2004@gmail.com>
2024-09-03 19:57:58 +0300

    cache settings

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
+from contextlib import asynccontextmanager
+
+import aioredis
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+
+from src.settings import SETTINGS
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """
+    Lifespan func for application
+    :param app:
+    :return:
+    """
+    redis = aioredis.from_url(
+        SETTINGS.AIOREDIS_URL.unicode_string(),
+        max_connections=20,
+        encoding="utf8",
+        decode_responses=True,
+        socket_connect_timeout=5,
+        socket_timeout=5,
+    )
+    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache", expire=60)
+    yield
+
 
 # Create FastAPI app
 app = FastAPI(
@@ -7,4 +34,5 @@ app = FastAPI(
     summary="",
     description="",
     default_response_class=ORJSONResponse,
+    lifespan=lifespan,
 )
--- src/settings.py
+++ src/settings.py
@@ -4,4 +4,4 @@ from src.schemas.settings import Settings
 # Base path to app
 BASE_DIR = Path(__file__).resolve().parent.parent
 # Initial Base settings schema instance
-SETTINGS = Settings()
+SETTINGS = Settings()  # type: ignore
