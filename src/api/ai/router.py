aed125ea97387810cc95d9325a0c4d0876568859
magelick <fcdm2004@gmail.com>
2024-09-21 12:53:24 +0300

    some changes in ai work

--- src/api/ai/router.py
+++ src/api/ai/router.py
@@ -1,9 +1,6 @@
 from fastapi import APIRouter, WebSocket
 from fastapi.responses import ORJSONResponse
-from starlette.websockets import WebSocketDisconnect
-
-from src.dependencies import ChatSession, ConnectionManager
-from src.gemini import generate_recommends, clean_and_format_text, create_chat
+from src.gemini import generate_recommends, clean_and_format_text
 
 router = APIRouter(
     prefix="/ai",
@@ -19,60 +16,32 @@ router = APIRouter(
 async def get_recommends(websocket: WebSocket):
     await websocket.accept()
     text = """
-    Проанализируй эти данные о ребенке и напиши меню на неделю для всей семьи с указанием  рецептов, количества ингредиентов и времени. Составь список продуктов, которые нужно купить с указанием количества, а также выведи сумму затрат на покупки в валюте локации: 12 лет; пол: мужской; рост: 150; вес: 60; ИМТ: не нужно; аллергии: цитрусовые; особенности питания и предпочтения: не любит грибы; хронические заболевания: диабет; локация: Беларусь; сезон: осень. Пример вывода информации:
+    Проанализируй эти данные о ребенке и напиши меню на неделю для всей семьи с указанием рецептов,
+    количества ингредиентов и времени: 12 лет; пол: мужской; рост: 150; вес: 60; ИМТ: не нужно;
+    аллергии: цитрусовые; особенности питания и предпочтения: не любит грибы; хронические заболевания: нет.
+    Составь список продуктов, которые нужно купить с указанием количества. Обязательно учитывай то,
+    что страна (регион) - Беларусь.
+    Пример вывода информации:
+
     Понедельник
-    Завтрак
-    «Контент»
-    Ингредиенты
-    Рецепт
-    Время приготовления
-    Блюдо ребёнка
-    Фото блюд
-    Обед
-    «Контент»
-    Ингредиенты
-    Рецепт
-    Время приготовления
-    Блюдо ребёнка
-    Фото блюд
-    Ужин
-    «Контент»
-    Ингредиенты
-    Рецепт
-    Время приготовления
-    Блюдо ребёнка
-    Фото блюд
-    Также для вторника - воскресенья.
+        Завтрак
+            Ингредиенты
+            Рецепт (способ приготовления)
+            Время приготовления
+
+        Обед
+            Ингредиенты
+            Рецепт (способ приготовления)
+            Время приготовления
+
+        Ужин
+            Ингредиенты
+            Рецепт (способ приготовления)
+            Время приготовления
+
+    Также для всех оставшихся дней недели.
     """
     async for chunk in generate_recommends(text=text):  # type: ignore
         await websocket.send_text(await clean_and_format_text(chunk))
 
     await websocket.close()
-
-
-@router.websocket(path="/chat", name="Create Chat with Gemini AI")
-async def chat_ai(
-    websocket: WebSocket, manager: ConnectionManager = ChatSession
-):
-    """
-
-    :param websocket:
-    :param manager:
-    :return:
-    """
-    # Подключение пользователя
-    await manager.connect(websocket)
-    try:
-        while True:
-            # Ожидание сообщения от клиента
-            data = await websocket.receive_text()
-
-            # Отправка запроса к Gemini AI
-            ai_response_generator = create_chat(data)
-
-            # Получаем ответы по частям и отправляем клиенту через WebSocket
-            async for chunk in ai_response_generator:
-                await manager.send_personal_message(chunk, websocket)
-    except WebSocketDisconnect:
-        # Отключение пользователя
-        manager.disconnect(websocket)
